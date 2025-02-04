3183e1e2e55c8d7bd962815fcb4d98e285673f4f
Deepak Mishra <official.deemis@outlook.com>
2025-02-04 19:23:20 +0530

    Add files via upload

---
+++ 1_helloworld.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print("Hello World 6")
+import flask
+print("enter first number")
+n1 = input()
+print("enter second number")
+n2 = input()
+print("the sum of these two numbers is", int(n1) + int(n2))
